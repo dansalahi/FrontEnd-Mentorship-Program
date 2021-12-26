f6c6b451f2e7b115707556e79d5583719def7233
danial <dev.salahi@gmail.com>
2021-12-26 21:40:13 +0300

    solved the palindrome algo

--- data-structures/cs-1/app.js
+++ data-structures/cs-1/app.js
@@ -1,1 +1,10 @@
-console.log("It's working!")
+console.log("It's working!");
+const palindrome = (str) => {
+  return str.split("").every((char, idx) => {
+    return char === str[str.length - idx - 1];
+  });
+};
+
+// const palindrome = (str) =>
+//   str.split("").every((char, idx) => char === str[str.length - idx - 1]);
+module.exports = palindrome;
